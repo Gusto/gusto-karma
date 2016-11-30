ee56c290f55544e3c72b10e5758694ed20dbabc3
Nikhil Mathew <nikhilmat@gmail.com>
2016-11-30 11:42:29 -0800

    Add json fixture configuration, consolidate paths

--- bin/gusto-karma.sh
+++ bin/gusto-karma.sh
@@ -4,14 +4,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 case "$1" in
   'test')
     echo 'Running Gusto-Karma Tests..'
-    karma start node_modules/gusto-karma/lib/karma.config.js
+    karma start node_modules/gusto-karma/lib/karma.config.js ${@:2:$#}
     ;;
   'server')
     echo 'Starting Gusto-Karma Server..'
-    karma start node_modules/gusto-karma/lib/karma.config.js --no-single-run
+    karma start node_modules/gusto-karma/lib/karma.config.js --no-single-run ${@:2:$#}
     ;;
+  'run')
+    echo "Running Gusto-Karma Tests for ${2}.."
+    karma run node_modules/gusto-karma/lib/karma.config.js  -- $2
   *)
-    echo 'Usage gusto-karma (profile|build|watch)'
+    echo 'Usage gusto-karma (test|server|)'
     exit 0
     ;;
 esac
