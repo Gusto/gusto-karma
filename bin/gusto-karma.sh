48b93daa11c048ee5ed8de307346e5ad7855e051
Nikhil Mathew <nikhilmat@gmail.com>
2017-01-06 19:50:31 -0800

    Fix enzyme warnings, stop test run on error

--- bin/gusto-karma.sh
+++ bin/gusto-karma.sh
@@ -4,15 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 case "$1" in
   'test')
     echo 'Running Gusto-Karma Tests..'
-    karma start node_modules/gusto-karma/lib/karma.config.js ${@:2:$#}
+    karma start node_modules/@gusto/gusto-karma/lib/karma.config.js ${@:2:$#}
     ;;
   'server')
     echo 'Starting Gusto-Karma Server..'
-    karma start node_modules/gusto-karma/lib/karma.config.js --no-single-run ${@:2:$#}
+    karma start node_modules/@gusto/gusto-karma/lib/karma.config.js --no-single-run ${@:2:$#}
     ;;
   'run')
     echo "Running Gusto-Karma Tests for ${2}.."
-    karma run node_modules/gusto-karma/lib/karma.config.js  -- $2
+    karma run node_modules/@gusto/gusto-karma/lib/karma.config.js  -- $2
     ;;
   *)
     echo 'Usage gusto-karma (test|server|run)'
